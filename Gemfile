749274fd3ff1ef4cf916280fbb9aa4d0737d5cb5
Jim <wefactoryou@gmail.com>
2017-11-22 20:27:38 -0800

    add avatars to users

--- Gemfile
+++ Gemfile
@@ -19,6 +19,7 @@ gem 'bootstrap-sass', '~> 3.3', '>= 3.3.7'
 gem 'sass-rails', '~> 5.0'
 
 gem 'devise', '~> 4.2'
+gem 'paperclip', '~> 5.1'
 
 group :development, :test do
   gem 'byebug', platform: :mri
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
-class User < ApplicationRecord
+class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   
   validates :fullname, presence: true, length: {maximum: 50}
+  
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
