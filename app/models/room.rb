77985ba3cf4b388b3d1c915e1264af61e4be778e
Jim <wefactoryou@gmail.com>
2017-11-25 21:20:57 -0800

    refactor room index page

--- app/models/room.rb
+++ app/models/room.rb
@@ -3,4 +3,12 @@ class Room < ApplicationRecord
   has_many :photos
   
   validates :room_type, presence: true
+  
+  def cover_photo(size)
+    if self.photos.length > 0
+      self.photos[0].image.url(size)
+    else
+      "blank.jpg"
+    end
+  end
 end
