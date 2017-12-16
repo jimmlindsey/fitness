a3681dccd9047be835d9e3a852d28546624e8339
Jim <wefactoryou@gmail.com>
2017-12-16 14:06:27 -0800

    add notification settings

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,6 +18,13 @@ class User < ActiveRecord::Base
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
   
+  has_one :setting
+  after_create :add_setting
+  
+  def add_setting
+    Setting.create(user: self, enable_sms: true, enable_email: true)
+  end
+  
   def self.from_omniauth(auth)
     user = User.where(email: auth.info.email).first
 
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,7 +12,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'Jim @ Company <no-reply@company.com>'
+  config.mailer_sender = 'Jimmy @ Evergym <no-reply@evergym.com>'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
